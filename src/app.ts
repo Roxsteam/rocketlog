fa52f3c75c7f3183930cbd20c8f3e284d29a0dc1
Roxsteam <neves.aleex@gmail.com>
2026-05-28 21:51:24 -0300

    Criando a rota e o controllers e organizando nossas rotas

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express"
 import "express-async-errors"
 
+import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
 
 const app = express()
 
 // Indicar que usaremos JSON
 app.use(express.json())
+
+app.use(routes)
 
 app.use(errorHandling)
 
--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction} from "express"
 import { AppError } from "@/utils/AppError"
+import { ZodError } from "zod"
 
 
 export function errorHandling (
@@ -8,9 +9,16 @@ request: Request,
 response: Response,
 next: NextFunction
 ) {
+// Tratamento de exceção
 if (error instanceof AppError) {
 return response.status(error.statusCode).json({ message: error.message })
     }
+// Tratamento de validação    
+if (error instanceof ZodError){
+return response.status(400).json({
+    message: "validation error",
+    issues: error.format(),
+})}
 
     return response.status(500).json({ message: error.message })
 }
