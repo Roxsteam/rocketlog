8e42d413284a2d2be893e471a7e315dd010fb31d
Roxsteam <neves.aleex@gmail.com>
2026-05-28 21:10:33 -0300

    Adicionando o middleware para tratamento de exceções

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express"
+import "express-async-errors"
+
+import { errorHandling } from "./middlewares/error-handling"
 
 const app = express()
 
 // Indicar que usaremos JSON
 app.use(express.json())
+
+app.use(errorHandling)
 
 export {app}
