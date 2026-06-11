8f9837d0b0de1d66196ffd6862258f7a7d763cac
Roxsteam <neves.aleex@gmail.com>
2026-06-10 21:53:42 -0300

    Criando controller das rotas e atualizando o status

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -6,14 +6,19 @@ import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
 
 import {verifyUserAuthorization} from "../middlewares/verifyUserAuthorizations"
 
+import { DeliveriesStatusController } from "@/controllers/deliveries-status"
 
 const deliveriesRoutes = Router()
 const deliveriesController = new DeliveriesController
-
+const deliveriesStatusController = new DeliveriesStatusController
 
 // todas as rotas passarão pelo fluxo de autenticação
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]))
 
 deliveriesRoutes.post("/", deliveriesController.create)
+deliveriesRoutes.get("/", deliveriesController.index)
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update )
+
 
 export { deliveriesRoutes }
